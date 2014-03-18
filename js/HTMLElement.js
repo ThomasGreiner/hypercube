cb607ca33c86f08fdeb40af122b1af346be4c5b7
Thomas Greiner <thomas@greinr.com>
2014-03-18 16:58:34 +0100

    Updated copyright information in license headers

--- js/HTMLElement.js
+++ js/HTMLElement.js
@@ -1,11 +1,11 @@
 /* 
- * Copyright 2012 Thomas Greiner
- * thomas@greinr.com
+ * Copyright 2012-2014 Thomas Greiner <http://www.greinr.com>
  *
  * The contents of this file are subject to the Artistic License 2.0.
  * The full text can be found in the LICENSE.txt file and at
  * http://www.opensource.org/licenses/artistic-license-2.0
  */
+
 HTMLElement.prototype.create = function(type) {
   var element = document.createElement(type);
   this.appendChild(element);
--- js/ui/Cluster.js
+++ js/ui/Cluster.js
@@ -1,11 +1,11 @@
 /* 
- * Copyright 2012 Thomas Greiner
- * thomas@greinr.com
+ * Copyright 2012-2014 Thomas Greiner <http://www.greinr.com>
  *
  * The contents of this file are subject to the Artistic License 2.0.
  * The full text can be found in the LICENSE.txt file and at
  * http://www.opensource.org/licenses/artistic-license-2.0
  */
+
 var Cluster = (function(id, name) {
   var _id = id;
   var _name = name;
