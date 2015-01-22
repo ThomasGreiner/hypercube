dd4c1f43a74903a9caa8140991bfd573f9738a6d
Thomas Greiner <thomas@greinr.com>
2015-01-22 23:06:27 +0100

    Reorganized code in ui/renderer

--- ui/renderer.js
+++ ui/renderer.js
@@ -8,53 +8,53 @@
 
 var nav = require("./lib/nav");
 
-var Renderer = new (function() {
-  var _html;
-  var _dominantColor = false;
-  var _dominantColorBrightness = false;
-  var _reInternalURL = /^(hypercube|about):(\/\/)?(start)$/;
+var Renderer = (function() {
+  var dominantColor = null;
+  var dominantColorBrightness = null;
+  var reInternalURL = /^(hypercube|about):(\/\/)?(start)$/;
   
-  this.__defineGetter__("dominantColor", function() {
-    return (_dominantColor) ? _dominantColor : [255, 255, 255, 1];
-  });
-  this.__defineSetter__("dominantColor", function(value) {
-    _dominantColor = value;
-  });
-  
-  this.__defineGetter__("dominantColorBrightness", function() {
-    if(_dominantColorBrightness) {
-      return _dominantColorBrightness;
-    }
-    
-    //calculate brightness
-    var color = this.dominantColor;
-    _dominantColorBrightness = Math.sqrt(0.241*Math.pow(color[0],2) + 0.691*Math.pow(color[1],2) + 0.068*Math.pow(color[2],2));
-    
-    return _dominantColorBrightness;
-  });
-  
-  this.init = function(html) {
-    _html = html;
-    
-    _html.addEventListener("load", function() {
-      nav.emit("pageloadend");
-    }, false);
-    
-    nav.on("locationchange", function(url) {
-      nav.emit("pageloadstart");
-      navigateTo(url);
-    }, false);
+  function calculateBrightness(color) {
+    return Math.sqrt(0.241 * Math.pow(color[0], 2)
+      + 0.691 * Math.pow(color[1], 2)
+      + 0.068 * Math.pow(color[2], 2));
   }
   
-  function navigateTo(url) {
-    if(_reInternalURL.test(url)) {
-      url = url.replace(_reInternalURL, "internal/$3.htm");
-    } else if(url.indexOf("://") == -1) {
-      url = "http://"+url;
+  var module = {
+    init: function(html) {
+      html.addEventListener("load", function() {
+        nav.emit("pageloadend");
+      }, false);
+      
+      nav.on("locationchange", function(url) {
+        nav.emit("pageloadstart");
+        
+        if (reInternalURL.test(url)) {
+          url = url.replace(reInternalURL, "internal/$3.htm");
+        } else if(url.indexOf("://") == -1) {
+          url = "http://" + url;
+        }
+        html.src = url;
+        
+        dominantColor = null;
+        dominantColorBrightness = null;
+      }, false);
     }
-    _html.src = url;
-    
-    _dominantColor = false;
-    _dominantColorBrightness = false;
-  }
+  };
+  
+  Object.defineProperties(module, {
+    dominantColor: {
+      get: function() {
+        return dominantColor || [255, 255, 255, 1];
+      },
+      set: function(value) {
+        dominantColor = value;
+      }
+    },
+    dominantColorBrightness: {
+      get: function() {
+        return dominantColorBrightness || calculateBrightness(module.dominantColor);
+      }
+    }
+  });
+  return module;
 })();
